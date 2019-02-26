505b94175f548cda2c025dcb9f28f20837af5322
SriHarsha300399 <harsha.akavaram@gmail.com>
2019-02-26 10:42:47 +0530

    User Module Update

--- EcommerceApp/src/main/java/com/dmantz/ecapp/controller/UserController.java
+++ EcommerceApp/src/main/java/com/dmantz/ecapp/controller/UserController.java
@@ -1,5 +1,25 @@
 package com.dmantz.ecapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dmantz.ecapp.request.CreateSignUpRequestPO;
+import com.dmantz.ecapp.service.UserManagerService;
+
+@RestController
 public class UserController {
+	
+	@Autowired
+	UserManagerService userManagerServiceObj;
+	
+	
+	@RequestMapping(value="/signUp",method=RequestMethod.POST)
+	public CreateSignUpRequestPO createSignUp(@RequestBody CreateSignUpRequestPO CreateSignUpRequestPOObj) {
+		
+		return userManagerServiceObj.createSignUp(CreateSignUpRequestPOObj);
+	}
 
 }
