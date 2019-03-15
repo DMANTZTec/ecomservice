7d00d73038595af56b6c71754c998005ff848eb5
MounikaSwarna98 <mounikaswarna98@gmail.com>
2019-03-15 14:41:00 +0530

    upadta in order service

--- EcommerceApp/src/main/java/com/dmantz/ecapp/controller/OrderController.java
+++ EcommerceApp/src/main/java/com/dmantz/ecapp/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.dmantz.ecapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,17 +14,28 @@ import com.dmantz.ecapp.service.OrderManagerService;
 @RestController
 @RequestMapping(value="/EcommerceApp")
 public class OrderController {
+	
 	@Autowired
-	OrderManagerService orderManagerServiceObj;
+	OrderManagerService orderManagerService;
 
-	@RequestMapping(value="/createOrder",method=RequestMethod.POST)
-	public OrderResponse createOrder(@RequestBody CreateOrderRequestPO createOrderRequestPOObj) {
+	@RequestMapping(value="/createOrder/{orderId}",method=RequestMethod.POST)
+	public OrderResponse createOrder(@PathVariable("orderId") int orderId,@RequestBody CreateOrderRequestPO createOrderRequestPO) {
+		System.out.println("oderid is"+orderId);
 		System.out.println("controller method called");
-		System.out.println("request object is"+createOrderRequestPOObj);
-		return orderManagerServiceObj.createOrder(createOrderRequestPOObj) ;
+		System.out.println("request object is"+createOrderRequestPO);
+		return orderManagerService.createOrder(orderId,createOrderRequestPO) ;
 		
 		
 		//this is done to learn git
 		//sorry mounika
+	}
+	
+	@RequestMapping(value="/createOrder2",method=RequestMethod.POST)
+	public OrderResponse createOrder2(@RequestBody CreateOrderRequestPO createOrderRequestPO) {
+
+
+		System.out.println("request object is"+createOrderRequestPO);		
+		return orderManagerService.createOrder2(createOrderRequestPO) ;
+		
 	}
 }
--- EcommerceApp/src/main/java/com/dmantz/ecapp/dao/OrderRepository.java
+++ EcommerceApp/src/main/java/com/dmantz/ecapp/dao/OrderRepository.java
@@ -2,8 +2,9 @@ package com.dmantz.ecapp.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.dmantz.ecapp.common.Order;
 import com.dmantz.ecapp.request.CreateOrderRequestPO;
 
-public interface OrderRepository extends JpaRepository<CreateOrderRequestPO,Integer> {
+public interface OrderRepository extends JpaRepository<Order,Integer> {
 
 }
